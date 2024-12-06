38c44cf8c0195df3643c0e1db00f0ede68d0e057
iamelikaa <melika.alamdar@gmail.com>
2024-12-06 12:23:24 +0100

    add the basic search engine no API

--- Src/script.js
+++ Src/script.js
@@ -1,1 +1,9 @@
-alert("hello world!");
+function handleSearch(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search_form_input");
+  let cityElement = document.querySelector("#weather_app_city");
+  cityElement.innerHTML = searchInput.value;
+}
+
+let searchFormElement = document.querySelector("#search_form");
+searchFormElement.addEventListener("submit", handleSearch);
